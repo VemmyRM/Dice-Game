346f1d59951baa11b311bed8663d1ef6cdc90e88
VemmyRM <kwvembu@gmail.com>
2021-02-12 16:44:51 -0500

    changing styling and adding naming functionality

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ var dice1 = document.getElementsByClassName("img1")[0];
 var dice2 = document.getElementsByClassName("img2")[0];
 var title = document.getElementsByTagName("h1")[0];
 var button = document.getElementsByTagName("button")[0];
+var player1 = document.getElementById("player1");
+var player2 = document.getElementById("player2");
+
 var image1 = "images/dice" + number1 + ".png";
 var image2 = "images/dice" + number2 + ".png";
 dice1.setAttribute("src", image1);
 dice2.setAttribute("src", image2);
 if (number1 > number2) {
-  title.textContent = "Player 1 Wins!";
+  title.textContent = player1.textContent +  " Wins!";
 } else if (number2 > number1) {
-  title.textContent = "Player 2 Wins!";
+  title.textContent = player2.textContent + " Wins!";
 } else {
   title.textContent = "Draw!";
 }
